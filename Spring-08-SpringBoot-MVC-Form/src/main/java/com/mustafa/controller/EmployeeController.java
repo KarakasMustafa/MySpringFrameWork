2dcc18bbd107fb24b038aa4e1ff002fe5a3e09d0
KarakasMustafa <m.isik0229@gmail.com>
2020-11-18 19:50:34 -0600

    created employee list page

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/mustafa/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/mustafa/controller/EmployeeController.java
@@ -6,8 +6,12 @@ import com.mustafa.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/employee")
@@ -18,13 +22,18 @@ public class EmployeeController {
 
         model.addAttribute("employee", new Employee());
         model.addAttribute("stateList", DataGenerator.getStateList());
+
+
         return "employee/employee-create";
     }
 
     @PostMapping("/list")
-    public String employeeList(){
+    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
 
+        model.addAttribute("employeeList", Arrays.asList(employee));
 
+        int birthYear = LocalDate.parse(employee.getBirthday()).getYear();
+        model.addAttribute("age",LocalDate.now().getYear() - birthYear);
         return "employee/employee-list";
     }
 }
