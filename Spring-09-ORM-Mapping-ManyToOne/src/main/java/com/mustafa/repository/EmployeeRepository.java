735dfe5a85dbe573661b89fa962103b9ff1b3ed9
KarakasMustafa <m.isik0229@gmail.com>
2020-12-18 14:36:20 -0600

    Added implementations

---
+++ Spring-09-ORM-Mapping-ManyToOne/src/main/java/com/mustafa/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.mustafa.repository;
+
+import com.mustafa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
