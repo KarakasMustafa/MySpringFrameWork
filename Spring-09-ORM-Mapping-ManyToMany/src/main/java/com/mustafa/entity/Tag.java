915def890fe4fe1dfeb8a3046e5f5d97d0eaff5f
KarakasMustafa <m.isik0229@gmail.com>
2020-12-19 11:17:33 -0600

    Created Tables with Many To Many relation

--- Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/mustafa/entity/Tag.java
+++ Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/mustafa/entity/Tag.java
@@ -4,10 +4,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -21,8 +20,10 @@ public class Tag {
 
     private String name;
 
-    public Tag(Long id, String name) {
-        this.id = id;
+    @ManyToMany(mappedBy = "tags",cascade = CascadeType.ALL)
+    private Set<Post> posts = new HashSet<>();
+
+    public Tag( String name) {
         this.name = name;
     }
 }
