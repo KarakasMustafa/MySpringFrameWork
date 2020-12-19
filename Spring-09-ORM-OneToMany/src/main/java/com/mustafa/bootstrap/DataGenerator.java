909acf981995b75a18eb8f3be6a9772c2e9f134f
KarakasMustafa <m.isik0229@gmail.com>
2020-12-19 10:40:30 -0600

    many to one

--- Spring-09-ORM-OneToMany/src/main/java/com/mustafa/bootstrap/DataGenerator.java
+++ Spring-09-ORM-OneToMany/src/main/java/com/mustafa/bootstrap/DataGenerator.java
@@ -2,16 +2,22 @@ package com.mustafa.bootstrap;
 
 import com.mustafa.entity.Address;
 import com.mustafa.entity.Person;
+import com.mustafa.repository.AddressRepository;
 import com.mustafa.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
 
     @Autowired
     PersonRepository personRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -25,6 +31,17 @@ public class DataGenerator implements CommandLineRunner {
         Address a2 = new Address("Elm St","25687");
         Address a3 = new Address("Task St","03256");
 
+    //    p1.setAddresses(Arrays.asList(a1,a2));      case 1
+
+        personRepository.save(p1);
+
+        a1.setPerson(p1);
+        a2.setPerson(p1);
+        a3.setPerson(p1);
+
+        addressRepository.save(a1);
+        addressRepository.save(a2);
+        addressRepository.save(a3);
 
     }
 }
